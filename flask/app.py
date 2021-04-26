39bc86c5244c66ba13f03627282a6c18ba78d1f5
NathanDuPont <nathan.dupont01@gmail.com>
2021-04-26 13:49:44 -0500

    Added flask components

---
+++ flask/app.py
@@ -0,0 +1,51 @@
+import os
+from .. import CycleGAN
+from flask import Flask, render_template
+from flask_uploads import UploadSet, configure_uploads, IMAGES, patch_request_class
+from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileRequired, FileAllowed
+from wtforms import SubmitField
+
+# python -m flask run
+# based on - https://gist.github.com/greyli/81d7e5ae6c9baf7f6cdfbf64e8a7c037
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'secret!'
+app.config['UPLOADED_PHOTOS_DEST'] = os.path.join(basedir, 'uploads')
+
+photos = UploadSet('photos', IMAGES)
+configure_uploads(app, photos)
+patch_request_class(app)  # set maximum file size, default is 16MB
+
+vangogh_model = CycleGAN.CycleGAN('style_vangogh_pretrained')
+
+class UploadForm(FlaskForm):
+    photo = FileField(validators=[FileAllowed(photos, 'Image only!'), FileRequired('File was empty!')])
+    submit = SubmitField('Upload')
+
+@app.route('/', methods=['GET', 'POST'])
+def upload_file():
+    form = UploadForm()
+    if form.validate_on_submit():
+        filename = photos.save(form.photo.data)
+        file_url = photos.url(filename)
+    
+        # Get output file
+        fname = filename.split(".")
+        out_fname = fname[0] + "_updated"
+        out_fname_full = out_fname + "." + fname[1]
+        out_url = str(file_url).replace(filename, out_fname_full)
+
+        # Detect face
+        faceDetector = fd.FaceDetector('model/deploy.prototxt.txt', 'model/opencv_face_detector.caffemodel')
+        fd.export_image_to_file('uploads/' + out_fname, faceDetector.detect_face_from_image(filename))
+    else:
+        file_url = None
+        out_url = None
+    return render_template('index.html', form=form, file_url=file_url, out_url=out_url)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ flask/readme.txt
@@ -0,0 +1,7 @@
+Better instructions to follow soon
+
+Install flask and other dependencies in the app.py file
+
+Run python -m flask run
+
+Upload an image then a face is detected by Kiel's stuff and eventually the outputed image is saved into the uploads folder for access and viewing
